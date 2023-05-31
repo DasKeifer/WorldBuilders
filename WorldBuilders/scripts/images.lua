85e6e478c9b79b83f5ef983f5e945cfcb5670a6d
Das Keifer <EplanumProductions@gmail.com>
2023-05-31 01:05:10 -0400

    First cut at image to cover forest fire burn symbol for mold

--- WorldBuilders/scripts/images.lua
+++ WorldBuilders/scripts/images.lua
@@ -4,3 +4,7 @@ local WorldBuilders_ResourcePath = mod_loader.mods[modApi.currentMod].resourcePa
 modApi:appendAsset("img/weapons/prime_wb_mold.png",    WorldBuilders_ResourcePath .. "img/weapons/prime_wb_mold.png")
 modApi:appendAsset("img/weapons/brute_wb_consume.png", WorldBuilders_ResourcePath .. "img/weapons/brute_wb_consume.png")
 modApi:appendAsset("img/weapons/science_wb_shift.png", WorldBuilders_ResourcePath .. "img/weapons/science_wb_shift.png")
+
+
+modApi:appendAsset("img/combat/icons/icon_forest_burn_cover.png", WorldBuilders_ResourcePath.."img/combat/icons/icon_forest_burn_cover.png")
+Location["combat/icons/icon_forest_burn_cover.png"] = Point(-9, 19)
