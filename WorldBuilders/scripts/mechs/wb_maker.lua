5daefb15aba27b23a34723595926483437cb77b3
Das Keifer <EplanumProductions@gmail.com>
2023-07-02 00:15:38 -0400

    Added in first cut of molder mech sprites

--- WorldBuilders/scripts/mechs/wb_maker.lua
+++ WorldBuilders/scripts/mechs/wb_maker.lua
@@ -7,13 +7,13 @@ local mod = modApi:getCurrentMod()
 local squadColors = modApi:getPaletteImageOffset("worldbuilders_color")
 
 local files = {
-	"th_entborg.png",
-	"th_entborg_a.png",
-	"th_entborg_w.png",
-	"th_entborg_w_broken.png",
-	"th_entborg_broken.png",
-	"th_entborg_ns.png",
-	"th_entborg_h.png"
+	"wb_maker.png",
+	"wb_maker_a.png",
+	"wb_maker_w.png",
+	"wb_maker_w_broken.png",
+	"wb_maker_broken.png",
+	"wb_maker_ns.png",
+	"wb_maker_h.png"
 }
 
 for _, file in ipairs(files) do
@@ -21,12 +21,12 @@ for _, file in ipairs(files) do
 end
 
 local a = ANIMS
-a.wb_maker =         a.MechUnit:new{Image = "units/player/th_entborg.png",          PosX = -17, PosY = -11 }
-a.wb_makera =        a.MechUnit:new{Image = "units/player/th_entborg_a.png",        PosX = -17, PosY = -11, NumFrames = 4 }
-a.wb_makerw =        a.MechUnit:new{Image = "units/player/th_entborg_w.png",        PosX = -17, PosY = -9 }
-a.wb_maker_broken =  a.MechUnit:new{Image = "units/player/th_entborg_broken.png",   PosX = -17, PosY =  2 }
-a.wb_makerw_broken = a.MechUnit:new{Image = "units/player/th_entborg_w_broken.png", PosX = -20, PosY =  4 }
-a.wb_maker_ns =      a.MechIcon:new{Image = "units/player/th_entborg_ns.png" }
+a.wb_maker =         a.MechUnit:new{Image = "units/player/wb_maker.png",          PosX = -16, PosY = -11 }
+a.wb_makera =        a.MechUnit:new{Image = "units/player/wb_maker_a.png",        PosX = -16, PosY = -11, NumFrames = 4 }
+a.wb_makerw =        a.MechUnit:new{Image = "units/player/wb_maker_w.png",        PosX = -16, PosY = -6 }
+a.wb_maker_broken =  a.MechUnit:new{Image = "units/player/wb_maker_broken.png",   PosX = -16, PosY =  2 }
+a.wb_makerw_broken = a.MechUnit:new{Image = "units/player/wb_maker_w_broken.png", PosX = -16, PosY =  4 }
+a.wb_maker_ns =      a.MechIcon:new{Image = "units/player/wb_maker_ns.png" }
 
 
 WorldBuilders_MakerMech = Pawn:new{	
